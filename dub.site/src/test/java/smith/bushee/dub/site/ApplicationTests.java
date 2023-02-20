411d40c0354a066dbb6e345e235ebbe541968d21
bms1040 <berna@Bradley-PC>
2023-02-20 11:10:44 -0500

    ...first commit

---
+++ dub.site/src/test/java/smith/bushee/dub/site/ApplicationTests.java
@@ -0,0 +1,13 @@
+package smith.bushee.dub.site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
